76ac83d44fc8983cd58f29e83cfec1373894ef2f
Tom Kraak <tom@backroom.io>
2016-05-30 23:04:43 -0400

    combine script into single file and refactor code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,53 @@
-var parent = document.querySelector("ul");
-var myNavitems = document.querySelectorAll('li a');
+/**
+ * use an immediately invoked function expression (IIFE) to create variable scope
+ * this way, the variables won't affect the surrounding code outside the IIFE
+ *
+ * it's now safe to omit semicolons, not sure how I feel about it yet
+ */
 
-parent.addEventListener("click", handler, false);
+(function () {
 
-function handler(e) {
+  var navParent = document.querySelector('ul')
+  var navItems = document.querySelectorAll('li a')
 
-  if (e.target !== e.currentTarget) {
+  var open = false
+  var contactBar = document.getElementById('contact-bar')
+  var contactNavItem = document.querySelector("nav [href='#contact']")
 
-    for(i = 0; i < myNavitems.length; i++) {
-      myNavitems[i].classList.remove('active');
+  navParent.addEventListener('click', setActiveColor, false)
+  contactNavItem.addEventListener('click', showHideContactBar, false)
+
+  function setActiveColor(e) {
+    if (e.target !== e.currentTarget) {
+
+    for (i = 0; i < navItems.length; i++) {
+      navItems[i].classList.remove('active')
     }
 
-    e.target.classList.add('active');
-   }
+    e.target.classList.add('active')
 
-  e.stopPropagation();
-}
+    }
+
+    e.stopPropagation()
+  }
+
+  function showHideContactBar() {
+    if (!open) {
+      openNav()
+    } else {
+      closeNav()
+    }
+  }
+
+  function openNav() {
+    open = true
+    contactBar.classList.remove('hide-contact-bar')
+    contactBar.classList.add('show-contact-bar')
+  }
+
+  function closeNav() {
+    open = false
+    contactBar.classList.remove ('show-contact-bar')
+    contactBar.classList.add('hide-contact-bar')
+  }
+})()
