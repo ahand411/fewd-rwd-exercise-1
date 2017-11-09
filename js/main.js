9be5ba8be78e62241189b6eddbecefa06f06c46f
Tom Kraak <tom.kraak@gmail.com>
2017-11-09 18:00:23 -0500

    refactor for ES6

--- js/main.js
+++ js/main.js
@@ -1,37 +1,27 @@
-/**
- * use an immediately invoked function expression (IIFE) to create variable scope
- * this way, the variables won't affect the surrounding code outside the IIFE
- *
- * it's now safe to omit semicolons, not sure how I feel about it yet
- */
-
 (function () {
+  const navParent = document.querySelector('ul')
+  const navItems = document.querySelectorAll('li a')
 
-  var navParent = document.querySelector('ul')
-  var navItems = document.querySelectorAll('li a')
-
-  var open = false
-  var contactBar = document.getElementById('contact-bar')
-  var contactNavItem = document.querySelector("nav [href='#contact']")
+  let open = false
+  const contactBar = document.getElementById('contact-bar')
+  const contactNavItem = document.querySelector("nav [href='#contact']")
 
   navParent.addEventListener('click', setActiveColor, false)
   contactNavItem.addEventListener('click', showHideContactBar, false)
 
-  function setActiveColor(e) {
+  function setActiveColor (e) {
     if (e.target !== e.currentTarget) {
+      for (let i = 0; i < navItems.length; i++) {
+        navItems[i].classList.remove('active')
+      }
 
-    for (i = 0; i < navItems.length; i++) {
-      navItems[i].classList.remove('active')
-    }
-
-    e.target.classList.add('active')
-
+      e.target.classList.add('active')
     }
 
     e.stopPropagation()
   }
 
-  function showHideContactBar() {
+  function showHideContactBar () {
     if (!open) {
       openNav()
     } else {
@@ -39,15 +29,15 @@
     }
   }
 
-  function openNav() {
+  function openNav () {
     open = true
     contactBar.classList.remove('hide-contact-bar')
     contactBar.classList.add('show-contact-bar')
   }
 
-  function closeNav() {
+  function closeNav () {
     open = false
-    contactBar.classList.remove ('show-contact-bar')
+    contactBar.classList.remove('show-contact-bar')
     contactBar.classList.add('hide-contact-bar')
   }
 })()
