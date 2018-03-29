0b2a2f4c9caa4706feef63eae8b8c6a8a03bff66
Tom Kraak <thomas.kraak@teamaol.com>
2018-03-29 17:22:31 -0400

    add semicolons

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 (function () {
-  const navParent = document.querySelector('ul')
-  const navItems = document.querySelectorAll('li a')
+  const navParent = document.querySelector('ul');
+  const navItems = document.querySelectorAll('li a');
 
-  let open = false
-  const contactBar = document.getElementById('contact-bar')
-  const contactNavItem = document.querySelector("nav [href='#contact']")
+  let open = false;
+  const contactBar = document.getElementById('contact-bar');
+  const contactNavItem = document.querySelector("nav [href='#contact']");
 
-  navParent.addEventListener('click', setActiveColor, false)
-  contactNavItem.addEventListener('click', showHideContactBar, false)
+  navParent.addEventListener('click', setActiveColor, false);
+  contactNavItem.addEventListener('click', showHideContactBar, false);
 
   function setActiveColor (e) {
     if (e.target !== e.currentTarget) {
       for (let i = 0; i < navItems.length; i++) {
-        navItems[i].classList.remove('active')
+        navItems[i].classList.remove('active');
       }
 
-      e.target.classList.add('active')
+      e.target.classList.add('active');
     }
 
-    e.stopPropagation()
+    e.stopPropagation();
   }
 
   function showHideContactBar () {
     if (!open) {
-      openNav()
+      openNav();
     } else {
-      closeNav()
+      closeNav();
     }
   }
 
   function openNav () {
-    open = true
-    contactBar.classList.remove('hide-contact-bar')
-    contactBar.classList.add('show-contact-bar')
+    open = true;
+    contactBar.classList.remove('hide-contact-bar');
+    contactBar.classList.add('show-contact-bar');
   }
 
   function closeNav () {
-    open = false
-    contactBar.classList.remove('show-contact-bar')
-    contactBar.classList.add('hide-contact-bar')
+    open = false;
+    contactBar.classList.remove('show-contact-bar');
+    contactBar.classList.add('hide-contact-bar');
   }
-})()
+})();
