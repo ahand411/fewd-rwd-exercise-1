50781999440fe5b7334d54db1bc39413f09f7f18
Tom Kraak <tom.kraak@gmail.com>
2018-10-07 12:36:59 -0400

    slight cleanup refactor

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
 (function () {
   const navParent = document.querySelector('ul');
-  const navItems = document.querySelectorAll('li a');
+  const navItems = navParent.querySelectorAll('li a');
 
   let open = false;
   const contactBar = document.getElementById('contact-bar');
-  const contactNavItem = document.querySelector("nav [href='#contact']");
+  const contactNavItem = navParent.querySelector("[href='#contact']");
 
-  navParent.addEventListener('click', setActiveColor, false);
-  contactNavItem.addEventListener('click', showHideContactBar, false);
+  navParent.addEventListener('click', setActiveColor);
+  contactNavItem.addEventListener('click', showHideContactBar);
 
   function setActiveColor (e) {
-    if (e.target !== e.currentTarget) {
-      for (let i = 0; i < navItems.length; i++) {
-        navItems[i].classList.remove('active');
-      }
-
-      e.target.classList.add('active');
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].classList.remove('active');
     }
 
+    e.target.classList.add('active');
     e.stopPropagation();
   }
 
